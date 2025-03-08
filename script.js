1ca5c6db6732ee6e43eabf1e0b2150a00e7d8b2d
sebitas71133 <jesus.huamanculi1@unmsm.edu.pe>
2025-03-07 21:48:04 -0500

    updated

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
+let bio = document.querySelector("#biografia");
+let cont = document.querySelector("#contribuciones");
+let fila = document.querySelector("#filantropia");
+let cita = document.querySelector("#citas");
+let btnBio = document.querySelector("#btnBiografia");
+let btnCont = document.querySelector("#btnContribuciones");
+let btnFila = document.querySelector("#btnFilantropia");
+let btnCita = document.querySelector("#btnCitas");
 
-let bio = document.querySelector('#biografia');
-let cont = document.querySelector('#contribuciones');
-let fila = document.querySelector('#filantropia');
-let cita = document.querySelector('#citas');
-let btnBio = document.querySelector('#btnBiografia');
-let btnCont = document.querySelector('#btnContribuciones');
-let btnFila = document.querySelector('#btnFilantropia');
-let btnCita = document.querySelector('#btnCitas');
-
-let array = [bio,cont,fila,cita];
+let array = [bio, cont, fila, cita];
 
 window.addEventListener("load", actualizar);
 
-function actualizar(){
-    cont.setAttribute('style',"display: none");
-    fila.setAttribute('style',"display: none");
-    cita.setAttribute('style',"display: none");
+function actualizar() {
+  cont.setAttribute("style", "display: none");
+  fila.setAttribute("style", "display: none");
+  cita.setAttribute("style", "display: none");
 }
 
-function tarjetas(e){
-    
-    array.forEach(tarjeta => {
-        
-        if(tarjeta.id == e.textContent.toLowerCase()){
-            tarjeta.setAttribute('style',"display:flex");
-        }else{
-            tarjeta.setAttribute('style',"display: none");
-        }
-        
-    } )
-    
+function tarjetas(e) {
+  array.forEach((tarjeta) => {
+    if (tarjeta.id === e.textContent.toLowerCase().trim()) {
+      tarjeta.setAttribute("style", "display:flex");
+    } else {
+      tarjeta.setAttribute("style", "display: none");
+    }
+  });
 }
